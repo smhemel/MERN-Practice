3f5929a75abdf77552e34d88ca32c0dab9657d21
S M HEMEL <sm_hemel@yahoo.com>
2019-10-15 11:40:27 +0600

    model created

---
+++ project-1/backend/models/user.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Shema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+},
+{
+    timestamps: true
+});
+
+
+module.exports = User = mongoose.model('users', userSchema);
